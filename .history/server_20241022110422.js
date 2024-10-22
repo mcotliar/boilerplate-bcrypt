6d20bf950c9fa1e0d91aab96a86328efad719901
mcotliar <m.cotliar+github@gmail.com>
2024-10-22 13:14:44 +0000

    bcrypt sync

--- .history/server_20241022110422.js
+++ .history/server_20241022110422.js
@@ -10,7 +10,7 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, 13, (err, hash) => {
     console.log(hash);
     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
@@ -19,7 +19,7 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 });
 
 
-bcrypt.hash(someOtherPlaintextPassword, saltRounds, (err, hash) => {
+bcrypt.hash(someOtherPlaintextPassword, 13, (err, hash) => {
     console.log(hash);
     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
     bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
